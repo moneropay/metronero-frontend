b7a27e66ef31fe7446d3c814d8f34f371ab65b86
Siren <siren@kernal.eu>
2023-03-02 21:27:42 +0200

    Use fiber

--- main.go
+++ main.go
@@ -1,60 +1,42 @@
 package main
 
 import (
-	"context"
 	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/gofiber/template/html"
 )
 
-func initRouter() *chi.Mux {
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	return r
-}
-
-func Run() {
-	h2s := &http2.Server{}
-	srv := &http.Server{
-		Addr: ":3000",
-		Handler: h2c.NewHandler(initRouter(), h2s),
-	}
-	serverCtx, serverStopCtx := context.WithCancel(context.Background())
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	go func() {
-		<-sig
-		shutdownCtx, _ := context.WithTimeout(serverCtx, 30 * time.Second)
-		go func() {
-			<-shutdownCtx.Done()
-			if shutdownCtx.Err() == context.DeadlineExceeded {
-				log.Fatal("Graceful shutdown timed out. Forcing exit.")
-			}
-		}()
-		err := srv.Shutdown(shutdownCtx)
-		if err != nil {
-			log.Fatal(err)
-		}
-		serverStopCtx()
-	}()
-	err := srv.ListenAndServe()
-	if err != nil && err != http.ErrServerClosed {
-		log.Fatal(err)
-	}
-	<-serverCtx.Done()
-}
-
 func main() {
-	Run()
+
+	// Create a new engine by passing the template folder
+	// and template extension using <engine>.New(dir, ext string)
+	engine := html.New("./template", ".html")
+
+	// Reload the templates on each render, good for development
+	engine.Reload(true) // Optional. Default: false
+
+	// Debug will print each template that is parsed, good for debugging
+	engine.Debug(true) // Optional. Default: false
+
+	// Layout defines the variable name that is used to yield templates within layouts
+	engine.Layout("embed") // Optional. Default: "embed"
+
+	// After you created your engine, you can pass it to Fiber's Views Engine
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Static("/", "./static")
+
+	// To render a template, you can call the ctx.Render function
+	// Render(tmpl string, values interface{}, layout ...string)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("merchant-dashboard", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
